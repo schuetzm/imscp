03ad6f2aabe60ce9945b4966834e6929a24e94df
Marc Schütz <schuetzm@gmx.net>
2011-10-31 19:42:16 +0100

    More services need restart on SuSE Linux

--- engine/setup/imscp-setup-methods.pl
+++ engine/setup/imscp-setup-methods.pl
@@ -939,8 +939,8 @@ sub restart_services {
 		['CMD_IMSCPN',			'restart',	1],
 		['CMD_IMSCPD',			'restart',	1],
 		['CMD_CLAMD',			'reload',	1],
-		['CMD_POSTGREY',		'reload',	1],
-		['CMD_POLICYD_WEIGHT',	'reload',	0],
+		['CMD_POSTGREY',		'restart',	1],
+		['CMD_POLICYD_WEIGHT',		'restart',	0],
 		['CMD_AMAVIS',			'reload',	1]
 	);
 
